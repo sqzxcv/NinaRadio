8d34ba155c4c5250aec7cbcc10e8ee0109e8e59f
Xia Jun <xiajun@XiadeMacBook.local>
2017-09-22 11:37:10 +0800

    修改头像

--- pages/Index/index.js
+++ pages/Index/index.js
@@ -25,22 +25,38 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
+
+        // 登录
+        wx.login({
+            success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
             }
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
+        })
+        // 获取用户信息
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            // 可以将 res 发送给后台解码出 unionId
+                            app.globalData.userInfo = res.userInfo
+                            this.setData({
+                                userInfo: app.globalData.userInfo,
+                                hasUserInfo: true
+                            })
+
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
+                            if (app.userInfoReadyCallback) {
+                                app.userInfoReadyCallback(res)
+                            }
+                        }
+                    })
+                }
+            }
+        })
+        if (this.data.canIUse == false) {
             wx.getUserInfo({
                 success: res => {
                     app.globalData.userInfo = res.userInfo
@@ -319,7 +335,7 @@ Page({
         this.data.currentShowIndex = event.detail.current;
         wx.setNavigationBarTitle({
             title: this.data.catalogs[event.detail.current].catalog_name
-          })
+        })
     },
 
     /**
